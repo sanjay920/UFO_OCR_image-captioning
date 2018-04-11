d71572004eb0898e9a1637cd19aa6e64b3bf6e40
Sachin GB <gumblapu@usc.edu>
2018-04-11 16:53:55 -0700

    Updating py utility

--- py_utility.py
+++ py_utility.py
@@ -7,43 +7,39 @@ import os
 IMAGE_CACHE_FILE_PAV = "image_cache_v2_prerana.txt"
 IMAGE_CACHE_FILE = "image_cache_v2.txt"
 IMAGE_URLS = "img_url.txt"
+MATTMANN_URL = "ufo_stalker_urls.csv"
 
-image_map = json.load(open(IMAGE_CACHE_FILE))
-image_map_pav = json.load(open(IMAGE_CACHE_FILE_PAV))
+# image_map = json.load(open(IMAGE_CACHE_FILE))
+#
+# new_image_map = {}
+#
+# unique_urls = set()
+# case_id = sorted(image_map.keys())
+# print case_id[0], case_id[len(case_id)-1]
+# num_photo = 0
+#
+# for key_id, val in image_map.items():
+#     num_photo += len(val['photos'])
+#     # for photo in val['photos']:
+#     #     with open(IMAGE_URLS, 'a+') as f:
+#     #         f.write(photo + '\n')
+#     for photo_url in val['photos']:
+#         if photo_url not in unique_urls:
+#             unique_urls.add(photo_url)
+#             # with open(IMAGE_URLS, 'a+') as f:
+#             #     f.write(photo_url + '\n')
 
-new_image_map = {}
+with open(MATTMANN_URL) as f:
+    content = f.readlines()
 
-unique_urls = set()
-case_id = sorted(image_map.keys())
-print case_id[0], case_id[len(case_id)-1]
-num_photo = 0
+content = [x.strip() for x in content]
 
-for key_id, val in image_map.items():
-    new_image_map[key_id] = val
-
-for key_id, val in image_map_pav.items():
-    if key_id not in new_image_map:
-        new_image_map[key_id] = val
-# if 72296 in case_id:
-# print image_map['72296']
-
-for key_id, val in new_image_map.items():
-    num_photo += len(val['photos'])
-    # for photo in val['photos']:
-    #     with open(IMAGE_URLS, 'a+') as f:
-    #         f.write(photo + '\n')
-    for photo_url in val['photos']:
-        if photo_url not in unique_urls:
-            unique_urls.add(photo_url)
-            # with open(IMAGE_URLS, 'a+') as f:
-            #     f.write(photo_url + '\n')
-
-def writeToCache(data):
-    with open(IMAGE_CACHE_FILE, 'w') as file:
+def writeToCache(data, file_name):
+    with open(file_name, 'w') as file:
         file.write(json.dumps(data))
 
-writeToCache(new_image_map)
+writeToCache({"all": list(content)}, "new_all_url.txt")
 
-print len(case_id)
-print num_photo
-print "unique_urls",len(unique_urls)
+# print len(case_id)
+# print num_photo
+# print "unique_urls",len(unique_urls)
